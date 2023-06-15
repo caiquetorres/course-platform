ac69a951ad048d047c9efb78cda24a90495df7bb
Caique Torres <caiquetorres.bastos@hotmail.com>
2023-06-15 17:06:43 -0300

    feat: made the ping route public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { Public } from './common/infrastructure/decorators/auth/public.decorator';
+
 @Controller()
 export class AppController {
+  @Public()
   @Get('ping')
   ping() {
     return 'pong';
